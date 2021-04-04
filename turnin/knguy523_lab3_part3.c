8df404943230fc2184c6a11efa5952195a1d1395
Kevin Nguyen <knguy523@wch136-22.cs.ucr.edu>
2021-04-04 02:17:30 -0700

    Completed part 5

--- turnin/knguy523_lab3_part3.c
+++ turnin/knguy523_lab3_part3.c
@@ -1,15 +1,12 @@
 /*	Author: Kevin Nguyen knguy523@ucr.edu
  *  Partner(s) Name: 
  *	Lab Section: 22
- *	Assignment: Lab #3  Exercise #2
- *	Exercise Description: A car has a fuel-level sensor that sets PA3..PA0 to a value between 0 (empty) 
- *		and 15 (full). A series of LEDs connected to PC5..PC0 should light to graphically indicate 
- *		the fuel level. If the fuel level is 1 or 2, PC5 lights. If the level is 3 or 4, PC5 
- *		and PC4 light. Level 5-6 lights PC5..PC3. 7-9 lights PC5..PC2. 10-12 lights PC5..PC1. 
- *		13-15 lights PC5..PC0. Also, PC6 connects to a "Low fuel" icon, which should light if 
- *		the level is 4 or less
- *	 
- *	 
+ *	Assignment: Lab #3  Exercise #3
+ *	Exercise Description: In addition to the above, PA4 is 1 if a key is in the ignition, PA5 is one 
+ *		if a driver is seated, and PA6 is 1 if the driver's seatbelt is fastened. PC7 should light 
+ *		a "Fasten seatbelt" icon if a key is in the ignition, the driver is seated, but the belt 
+ *		is not fastened. 
+ *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  */
